bf8315e474d69601c84137a07bba7148570ee7ef
katkadeomkar <katkadeomkar@gmail.com>
2023-01-25 23:29:40 +0530

    develope commit

--- src/Collection_Practice/VectorDemo.java
+++ src/Collection_Practice/VectorDemo.java
@@ -19,5 +19,18 @@ public class VectorDemo {
 		while (e.hasMoreElements()) {
 			System.out.println(e.nextElement());
 		}
+		
+		Vector v = new Vector();
+		v.add(10);
+		v.add(20);
+		v.add(30);
+		v.add('A');
+		v.add(null);
+		System.out.println("Vector elements are ");
+		Enumeration e = v.elements();
+
+		while (e.hasMoreElements()) {
+			System.out.println(e.nextElement());
 	}
+}
 }
